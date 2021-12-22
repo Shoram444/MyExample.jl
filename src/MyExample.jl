8b41bd55c9a4479dee71d96c3140e4dd629faf9c
Maros <petro.maros.afs@gmail.com>
2021-12-22 09:23:20 +0100

    aa

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,10 +1,6 @@
 module MyExample
 
-<<<<<<< HEAD
 include("file1.jl")
 export f
-=======
->>>>>>> main
-# Write your package code here.
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,8 @@
 using MyExample
 using Test
 
-<<<<<<< HEAD
 
 @testset "MyExample.jl" begin
     # Write your tests here.
     @test f(2) == 4   # I expect the return value of the function, so we "test" it.
-=======
-@testset "MyExample.jl" begin
-    # Write your tests here.
->>>>>>> main
 end
